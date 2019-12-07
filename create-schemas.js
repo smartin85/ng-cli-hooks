12c5174c6816bb09b04a29db088843063c266a05
Smartin <smartin@nematronic.de>
2019-12-07 17:47:51 +0100

    switched to create-schemas

---
+++ create-schemas.js
@@ -0,0 +1,47 @@
+var rootPath = process.cwd();
+    fs = require('fs'),
+	path = require('path'),	
+	defaultsDeep = require('lodash/defaultsDeep'),
+	angularSchemas = [
+		'browser',
+		'dev-server',
+		'extract-i18n',
+		'server'
+	],
+	ionicSchemas = [
+		'cordova-build'
+	];
+
+function createModifiedAngularSchema(name) {
+    const original = require(`@angular-devkit/build-angular/src/${name}/schema.json`),
+        modifications = require(`./src/${name}/schema.json`);
+        
+    createSchema(name, original, modifications);		
+}
+
+function createModifiedIonicSchema(name) {
+	const original = require(`@ionic/angular-toolkit/builders/cordova-build/schema.json`),
+		modifications = require(`./src/${name}/schema.json`);
+    
+    createSchema(name, original, modifications);
+}
+
+function createSchema(name, original, modifications) {
+    const result = defaultsDeep(original, modifications),
+        dest = path.resolve(rootPath, `./dist/${name}`),
+        data = JSON.stringify(result, null, 2);
+
+    if(fs.existsSync(dest)) {
+        fs.writeFile(`${dest}/schema.json`, data, err => {
+            if(err) {
+                throw err;
+            }
+            console.log(`Created ${dest}/schema.json`);
+        });        
+    } else {
+        console.log(`Path does not exist: ${dest}`);
+    }
+}
+
+angularSchemas.forEach(s => createModifiedAngularSchema(s));
+ionicSchemas.forEach(s => createModifiedIonicSchema(s));
