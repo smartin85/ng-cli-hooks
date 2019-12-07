435fa0817ee918658aba6fa4f36e61b1c6a87d2b
Smartin <smartin@nematronic.de>
2019-12-07 23:36:49 +0100

    target options

--- src/dev-server/index.ts
+++ src/dev-server/index.ts
@@ -1,24 +1,31 @@
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { BuilderContext, createBuilder } from '@angular-devkit/architect';
 import { json } from '@angular-devkit/core';
 import { DevServerBuilderOutput, executeDevServerBuilder, DevServerBuilderOptions } from '@angular-devkit/build-angular';
 import { modifyOptions, modifyWebpack, modifyIndexHtml } from '../modifiers';
 import { IHookableOptions } from '../IHookable';
+import { getTargetOptions } from '../utils';
+import { switchMap } from 'rxjs/operators';
 
 type DevServerSchema = IHookableOptions & DevServerBuilderOptions;
 
 export function serveBrowser(
 	options: DevServerSchema,
 	context: BuilderContext
-  ): Observable<DevServerBuilderOutput> {
-	return executeDevServerBuilder(
-		modifyOptions(options, context),
-		context,
-		{
-			webpackConfiguration: modifyWebpack(options, context),
-			indexHtml: modifyIndexHtml(options, context)
-		}
-	)
-  }
-  
-  export default createBuilder<json.JsonObject & DevServerSchema>(serveBrowser);
+): Observable<DevServerBuilderOutput> {
+	return from(getTargetOptions(options, context))
+		.pipe(
+			switchMap(targetOptions =>
+				executeDevServerBuilder(
+					modifyOptions(options, context),
+					context,
+					{
+						webpackConfiguration: modifyWebpack(targetOptions, context),
+						indexHtml: modifyIndexHtml(targetOptions, context)
+					}
+				)
+			)
+		);
+}
+
+export default createBuilder<json.JsonObject & DevServerSchema>(serveBrowser);
